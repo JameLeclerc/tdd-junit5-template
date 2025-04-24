0f41d4fa3e0aba522bcdf5568d0fe0f24c30d2a4
JameLeclerc <jl626591@wne.edu>
2025-04-24 17:45:49 +0000

    code: FEAT: add WardrobeCalc class using passed wardobe list and set parameter.

---
+++ src/main/java/com/WardrobeKata/project/WardrobeCalc.java
@@ -0,0 +1,9 @@
+package com.WardrobeKata.project;
+import java.util.List;
+
+public class WardrobeCalc {
+    private List<Wardrobe> wrdbs;
+    public WardrobeCalc(List<Wardrobe> wrdbs){
+        this.wrdbs = wrdbs;
+    }
+}
