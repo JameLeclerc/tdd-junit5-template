a05bd2b38db54bee792a96d7bc9d349d3d530181
JameLeclerc <jl626591@wne.edu>
2025-04-24 15:40:37 +0000

    code: FEAT: pass nothing test
    reformat template and add passing nothing test.

---
+++ src/main/java/com/WardrobeKata/project/Wardrobe.java
@@ -0,0 +1,5 @@
+package com.WardrobeKata.project;
+
+public class Wardrobe{
+
+}
--- src/test/java/com/WardrobeKata/project/WardrobeTests.java
+++ src/test/java/com/WardrobeKata/project/WardrobeTests.java
@@ -1,14 +1,4 @@
-/*
- * Copyright 2015-2025 the original author or authors.
- *
- * All rights reserved. This program and the accompanying materials are
- * made available under the terms of the Eclipse Public License v2.0 which
- * accompanies this distribution and is available at
- *
- * https://www.eclipse.org/legal/epl-v20.html
- */
-
-package com.example.project;
+package com.WardrobeKata.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,8 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class CalculatorTests {
+class WardrobeTests {
 
+	@Test
+	public void nothing(){
+
+	}
+	/*
 	@Test
 	@DisplayName("1 + 1 = 2")
 	void addsTwoNumbers() {
@@ -38,4 +33,5 @@ class CalculatorTests {
 		assertEquals(expectedResult, calculator.add(first, second),
 				() -> first + " + " + second + " should equal " + expectedResult);
 	}
+				*/
 }
