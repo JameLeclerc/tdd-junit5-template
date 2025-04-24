0970ce41b810068a4a37947236c7afff4067e1aa
JameLeclerc <jl626591@wne.edu>
2025-04-24 16:33:26 +0000

    code: FEAT: add abstract getter and size attribute
    add attribute size to abstract class and subclass(50 for sub class) and abstract getter for size attribute to be used in test.

--- src/main/java/com/WardrobeKata/project/Wardrobe.java
+++ src/main/java/com/WardrobeKata/project/Wardrobe.java
@@ -1,5 +1,9 @@
 package com.WardrobeKata.project;
 
 public abstract class Wardrobe{
-
+    private int size;
+    public Wardrobe(int size){
+        this.size = size;
+    }
+    public int getSize(){return size;}
 }
--- src/main/java/com/WardrobeKata/project/Wardrobe50.java
+++ src/main/java/com/WardrobeKata/project/Wardrobe50.java
@@ -1,5 +1,7 @@
 package com.WardrobeKata.project;
 
 public class Wardrobe50 extends Wardrobe{
-
+    public Wardrobe50(){
+        super(50);
+    }
 }
